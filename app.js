5eff68439d2f9b45834a5c020ef3da832759e75d
Audrey Alaime <audreyalaime3@gmail.com>
2023-05-10 15:20:56 +0200

    Quests-N°5-DELETE

--- app.js
+++ app.js
@@ -24,12 +24,14 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
 app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 // Route GET /api/users pour renvoyer tous les utilisateurs
 app.get("/api/users", users.getUsers);
 app.get("/api/users/:id", users.getUsersId);
 app.post('/api/users', validateUser, users.postUsers);
 app.put("/api/users/:id", validateUser, users.updateUsers);
+app.delete("/api/users/:id", users.deleteUsers);
 
 app.listen(port, (err) => {
   if (err) {
--- users.js
+++ users.js
@@ -120,11 +120,29 @@ const updateUsers = (req,res)=>{
     );
 };
 
+const deleteUsers = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("DELETE FROM users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the user");
+    });
+};
 
   module.exports = {
     getUsers,
     getUsersId,
     postUsers,
     updateUsers,
+    deleteUsers,
   };
   
